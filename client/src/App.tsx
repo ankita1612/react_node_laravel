06afa60c7700cb676e50e7ebbccfa3de9926faa9
ANKITA <ankita.mca2007@gmail.com>
2026-05-05 19:29:48 +0530

    changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,10 @@
 function App() {
   return (
-    <>
-      <div className="flex items-center justify-center h-screen bg-gray-100">
-        <h1 className="text-3xl font-bold text-red-600">
-          React + Tailwind + TS 🚀
-        </h1>
-        <div className="text-3xl font-bold text-red-600">
-          React + Tailwind + TS 🚀
-        </div>
-      </div>
-      <br></br>
-      <div className="text-sm">sds</div>
-      <br></br>
-      <div className="text-base">sds11</div>
-    </>
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-3xl font-bold text-blue-600">
+        React + Tailwind + TS 🚀
+      </h1>
+    </div>
   );
 }
 
