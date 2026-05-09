0f29d616cd341050f213f3c864bfea629661dada
ANKITA <ankita.mca2007@gmail.com>
2026-05-09 17:24:02 +0530

    changes

--- react_laravel/react_laravel_auth_crud/server/config/cors.php
+++ react_laravel/react_laravel_auth_crud/server/config/cors.php
@@ -5,10 +5,8 @@ return [
     'allowed_methods' => ['*'],
     
     'allowed_origins' => [
-        'http://localhost:5173',      // Vite dev server
-        'http://localhost:3000',      // Alternative
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:3000',
+        'http://localhost:5174',        
+        'http://127.0.0.1:5174',       
     ],
     
     'allowed_origins_patterns' => [],
--- react_laravel/react_laravel_auth_crud/server/config/sanctum.php
+++ react_laravel/react_laravel_auth_crud/server/config/sanctum.php
@@ -18,7 +18,7 @@ return [
     |
     */
 
-    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:5173,127.0.0.1:5173')),
+    'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', 'localhost:5174,127.0.0.1:5174')),
 
     /*
     |--------------------------------------------------------------------------
